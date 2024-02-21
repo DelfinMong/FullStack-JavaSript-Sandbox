9d235f6e43e73fa8f4825c6fde2c2f8d227033c7
DelfinMong <delfdevs@gmail.com>
2024-02-21 03:21:42 -0500

    console

---
+++ 01-Variables-data-types/01-console/script.js
@@ -0,0 +1,18 @@
+// Log number
+console.log(100)
+
+// Log String
+console.log('Hello World')
+
+// Log multiple values
+console.log(20,'Hello World', true)
+
+// Console error & warning
+console.log('Alert')
+console.warn('warning')
+
+// Log object as table
+console.table({
+  name:'brad',
+  email:'brad@gmail.com'
+})
